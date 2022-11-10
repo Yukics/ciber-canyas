9bf96152b88b340fb5de2d5edabfff59f892d4a8
Yukics <alejomorell@gmail.com>
2022-11-10 22:43:31 +0100

    postgres tables structure defined

--- backend/main.go
+++ backend/main.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	// "fmt"
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	    //initialises a router with the default functions.
-		router := gin.Default()
-		
-		router.GET("/", func(context *gin.Context) {
-			context.String(http.StatusOK, "Hello world!")
-		})
-    
-		// starts the server at port 8080
-		router.Run(":8080")
+	//initialises a router with the default functions.
+	router := gin.Default()
+
+	router.GET("/", func(context *gin.Context) {
+		context.String(http.StatusOK, "Hello world!")
+	})
+
+	router.POST("/login", func(context *gin.Context) {
+		context.String(http.StatusOK, "Hello world!")
+	})
+
+	router.POST("/logout", func(context *gin.Context) {
+		context.String(http.StatusOK, "Hello world!")
+	})
+
+	// starts the server at port 8080
+	router.Run(":8080")
 
 }
---
+++ database/init.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE canyes;
+
+\connect canyes;
+
+CREATE TABLE users (
+	user_id serial PRIMARY KEY,
+	mail VARCHAR ( 50 ) UNIQUE NOT NULL
+);
+
+CREATE TABLE sessions (
+    session_id serial PRIMARY KEY,
+    user_id INT NOT NULL,
+    expiration TIMESTAMP NOT NULL,
+    token VARCHAR ( 120 ) UNIQUE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+CREATE TABLE elements (
+    element_id serial PRIMARY KEY,
+    emoji VARCHAR ( 50 ) UNIQUE NOT NULL
+);
+
+CREATE TABLE interactions (
+    interaction_id serial PRIMARY KEY,
+    user_id INT NOT NULL,
+    element_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (element_id) REFERENCES elements (element_id)
+);
