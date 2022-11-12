e001fbbc0ea99d1a95b16be6cd05ccdf0c6733a4
Yukics <alejomorell@gmail.com>
2022-11-12 22:14:38 +0100

    refactored postgres init folder

---
+++ database/2.init-data.sql
@@ -0,0 +1,10 @@
+\connect canyes;
+
+-- Users inserts
+INSERT INTO users (user_id, mail) VALUES (DEFAULT, 'amorell@cifpfbmoll.eu');
+
+-- Emojis inserts
+-- Chupito
+INSERT INTO elements (element_id, emoji) VALUES (DEFAULT,'U+1F943'); 
+-- Canya
+INSERT INTO elements (element_id, emoji) VALUES (DEFAULT,'U+1F37A'); 
